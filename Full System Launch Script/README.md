84d86eeeb60f94508f1633052abe8dd5fe56c6ec
SouLeo <slwanna@utexas.edu>
2017-04-10 11:15:36 -0500

    launching script

---
+++ Full System Launch Script/README.md
@@ -0,0 +1,5 @@
+In order to use this script to launch the demo, make sure you have appropriately sourced your vector_ws.
+
+Other than that, either place the script in your HOME folder or Desktop. To run, type into the console:
+
+./run_senior_design_sum.sh
---
+++ Full System Launch Script/run_senior_design_sim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+roslaunch hlpr_gazebo vector.launch &
+pid1="$!"
+sleep 5 
+roslaunch vector_viz view_robot.launch function:=map_nav &
+pid2="$!"
+sleep 5
+roslaunch vector_navigation_apps 2d_map_nav_demo.launch map_file:=robuddy
+pid3="$!"
+
+wait
+
+trap 'echo Demo is shutting down. Killing child processes...; kill $(jobs -p); rosnode kill --all; killall -9 roscore; killall -9 rosmaster; exit' SIGHUP SIGINT SIGQUIT SIGTERM EXIT
+
+#EXIT
