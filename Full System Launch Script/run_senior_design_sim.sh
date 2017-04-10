d95b3c68b5f94b98688e3f64bef3734131a179be
SouLeo <slwanna@utexas.edu>
2017-04-10 11:37:52 -0500

    cleaned up code

--- Full System Launch Script/run_senior_design_sim.sh
+++ Full System Launch Script/run_senior_design_sim.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 roslaunch hlpr_gazebo vector.launch &
-pid1="$!"
 sleep 5 
-roslaunch vector_viz view_robot.launch function:=map_nav &
-pid2="$!"
-sleep 5
-roslaunch vector_navigation_apps 2d_map_nav_demo.launch map_file:=robuddy
-pid3="$!"
 
+roslaunch vector_viz view_robot.launch function:=map_nav &
+sleep 5
+
+roslaunch vector_navigation_apps 2d_map_nav_demo.launch map_file:=robuddy
 wait
 
 trap 'echo Demo is shutting down. Killing child processes...; kill $(jobs -p); rosnode kill --all; killall -9 roscore; killall -9 rosmaster; exit' SIGHUP SIGINT SIGQUIT SIGTERM EXIT
